04b801d626f9cc4c361cd488326a1fc7f5b59c9f
yawar2019 <yawarali17@gmail.com>
2021-04-26 07:58:46 +0530

    Committed

---
+++ AdoNetExample/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using AdoNetExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdoNetExample.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        Employee db = new Employee();
+        public ActionResult Index()
+        {
+            return View(db.getEmployees());
+        }
+    }
+}
---
+++ AdoNetExample/Models/EmployeeModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AdoNetExample.Models
+{
+    public class EmployeeModel
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpSalary { get; set; }
+    }
+    public class Employee
+    {
+        public List<EmployeeModel> getEmployees()
+        {
+            List<EmployeeModel> listObj = new List<Models.EmployeeModel>();
+            SqlConnection con = new SqlConnection("Data Source=AZAM-PC\\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=true");
+            SqlCommand cmd = new SqlCommand("sp_getNeerjaEmployees", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                EmployeeModel obj = new EmployeeModel();
+                obj.EmpId = Convert.ToInt32(dr[0]);
+                obj.EmpName = Convert.ToString(dr[1]);
+                obj.EmpSalary = Convert.ToInt32(dr[2]);
+                listObj.Add(obj);
+
+            }     
+
+            return listObj;
+        }
+    }
+
+}
